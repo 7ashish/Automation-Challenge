51ee76dc32d1300769bb5597568c525d97a12b21
7ashish <hashish.mh@gmail.com>
2021-06-20 20:04:07 +0200

    Maven Project Creation

---
+++ AutomationChallenge/src/main/java/Selenium/AutomationChallenge/App.java
@@ -0,0 +1,13 @@
+package Selenium.AutomationChallenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
